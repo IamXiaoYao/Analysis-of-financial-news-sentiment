68864d5cf63c39b1ab435c31c9a0e49808f1a8f1
xiao-yao-ryan <xiaoyao_ryan@163.com>
2024-12-21 02:30:51 +0800

    Format

--- Crawl_Data/yahoo_news_crawler.py
+++ Crawl_Data/yahoo_news_crawler.py
@@ -6,7 +6,6 @@ import Log
 import pandas as pd
 from lxml import etree
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
